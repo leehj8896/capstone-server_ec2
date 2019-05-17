991bd5930e5d7d9712d24cd8d078bb0594f67108
leehj8896 <leehj8896@gmail.com>
2019-05-17 05:04:49 +0000

    장소 등록 시 이벤트-장소 관계테이블에도 자동으로 관계 데이터 입력되도록 views.py 수정. 참여 버튼 클릭 시 participation 테이블에 데이터 입력되도록 수정.

--- print_db/urls.py
+++ print_db/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
         path('imply/', views.imply, name='imply'),
         path('insert_event/', views.insert_event, name='insert_event'),
         path('insert_place/', views.insert_place, name='insert_place'),
+        path('participate_event/', views.participate_event, name='participate_event'),
+        path("insert_user/", views.insert_user, name="insert_user"),        
+        path("insert_user_direct/", views.insert_user_direct, name="insert_user_direct"),
+        path("id_duplicate_check/", views.id_duplicate_check, name="id_duplicate_check"),
+        path("login/", views.login, name="login"),
 ]
--- print_db/views.py
+++ print_db/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from .models import *
+from django.shortcuts import render
 import MySQLdb
 
 def index(request):
@@ -40,13 +41,63 @@ def print_dicts(query):
 def insert_event(request):
     
     lastId = Event.objects.all().last().event_id
-    event = Event.objects.create(event_id = lastId + 1, event_name = request.POST['event_name'], reward = request.POST['reward'], user = User.objects.get(user_id = request.POST['user_id']))
+    event = Event.objects.create(event_id=lastId+1, event_name=request.POST['event_name'], reward=request.POST['reward'], user=User.objects.get(user_id=request.POST['user_id']))
     
     return HttpResponse()
+
 
 def insert_place(request):
 
     lastId = Place.objects.all().last().place_id
-    place = Place.objects.create(place_id = lastId + 1, place_name = request.POST['place_name'], address = request.POST['address'], explanation = request.POST['explanation'])
+    place = Place.objects.create(place_id=lastId+1, place_name=request.POST['place_name'], address=request.POST['address'], explanation=request.POST['explanation'])
+    imply = Imply.objects.create(event_id=Event.objects.all().last().event_id, place_id=Place.objects.all().last().place_id)
 
     return HttpResponse()
+
+def insert_user(request):
+    try:
+        
+        user = User.objects.create(user_id=request.POST.get('user_id'),user_password='',user_email='',user_information=request.POST['user_information'])
+        return HttpResponse('success')
+    except IndentationError:
+        return HttpResponse("There is duplicate id")
+    except Exception as e:
+       return HttpResponse(e)
+
+def insert_user_direct(request):
+    try:
+        user = User.objects.create(user_id=request.POST.get('user_id'),user_password=request.POST.get("user_password"),user_email='',user_information=request.POST['user_information'])
+        return HttpResponse('success')
+    except IndentationError:
+        return HttpResponse("There is duplicate id");
+    except Exception as e:
+        return HttpResponse(e)
+
+def id_duplicate_check(request):
+            id = request.POST['user_id']
+            query = 'select user_id, count(*) from user where user_id = "%s" group by user_id' % id
+            conn = MySQLdb.connect('127.0.0.1', 'root', '', 'capstone', charset='utf8')
+            curs = conn.cursor(MySQLdb.cursors.DictCursor)
+            curs.execute(query)
+            rowcount = curs.rowcount
+            if(rowcount ==0):
+                return HttpResponse('ok')
+            else:
+                return HttpResponse('error')
+
+def login(request):
+    id = request.POST['user_id']
+    password = request.POST['user_password']
+    query = 'select user_id, user_password from user where user_id="%s" and user_password="%s";' %(id,password)
+    conn = MySQLdb.connect('127.0.0.1', 'root', '', 'capstone', charset='utf8')
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute(query)
+    rowcount = curs.rowcount
+    if(rowcount == 0):
+        return HttpResponse('error')
+    else:
+        return HttpResponse('ok')
+
+def participate_event(request):
+
+    participation = Participation.objects.create(user=User.objects.get(user_id=request.POST['user_id']), event=Event.objects.get(event_id=request.POST['event_id']))
